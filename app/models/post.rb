b69628d90dbfab0beacbd2c1a563f2c00a75a785
xitrumuit1991 <tranvokhoinguyen@gmail.com>
2015-05-18 16:36:01 +0700

    a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
 	attr_accessible :title, :content
+
+
+	validates :title,  :content , :presence => true
+	validates :title, :length => { :minimum => 2 }
+	validates :title, :uniqueness => {:message =>"already taken"}
 end
